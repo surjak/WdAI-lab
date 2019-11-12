b691bb79856f084c506de4b98e35167731e991fd
surjak <surjak1999@gmail.com>
2019-11-12 16:55:00 +0100

    add and remove recipe done

--- CookBook/app.js
+++ CookBook/app.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector("#addRecipe");
 const recipes = document.querySelector(".recipes");
 let listofRecipes = document.querySelectorAll(".grid-item");
-function addRecipe() {
+function addRecipe(nazwa, opis, skladniki, url) {
   let recipe = document.createElement("div");
   let readmore = document.createElement("div");
   readmore.classList.add("readmore");
@@ -11,13 +11,16 @@ function addRecipe() {
   let btn = document.createElement("button");
   btn.classList.add("btn-remove");
   btn.innerHTML = "X";
+  btn.addEventListener("click", () => {
+    recipes.removeChild(recipe);
+  });
   let img = document.createElement("img");
   let div = document.createElement("div");
-  img.src = "./images/chef-gallery-2.jpg";
+  img.src = url;
   let h3 = document.createElement("h3");
   let p = document.createElement("p");
-  h3.innerHTML = "Pizza";
-  p.innerHTML = "ajjksajdjshdjkshjkadhashdjkashjkdhasjkdhn";
+  h3.innerHTML = nazwa;
+  p.innerHTML = opis;
   div.appendChild(h3);
   div.appendChild(p);
   div.appendChild(btn);
@@ -25,9 +28,6 @@ function addRecipe() {
   recipe.appendChild(readmore);
   recipe.appendChild(img);
   recipe.appendChild(div);
-  recipe.addEventListener("click", () => {
-    recipes.removeChild(recipe);
-  });
   recipes.appendChild(recipe);
 }
 
@@ -37,29 +37,29 @@ const tl = new TimelineMax({ paused: true, reversed: true });
 tl.fromTo(formularz, 0.5, { x: "100%" }, { opacity: 1, x: "0%" }, "-=0.1");
 
 btn.addEventListener("click", () => {
-  // addRecipe();
   tl.reversed() ? tl.play() : tl.reverse();
 });
 
-listofRecipes.forEach(recipe => {
-  recipe.addEventListener("click", () => {
-    recipes.removeChild(recipe);
+const listOfButtons = document.querySelectorAll(".btn-remove");
+listOfButtons.forEach(btn =>
+  btn.addEventListener("click", () => {
+    recipes.removeChild(btn.parentNode.parentNode);
+  })
+);
+
+const submitBtn = document
+  .querySelector("#submitBtn")
+  .addEventListener("click", e => {
+    e.preventDefault();
+    const opis = document.querySelector("#opis");
+    const skladniki = document.querySelector("#skladniki");
+    const url = document.querySelector("#url");
+    const nazwa = document.querySelector("#nazwa");
+    addRecipe(nazwa.value, opis.value, skladniki.value, url.value);
+
+    tl.reversed() ? tl.play() : tl.reverse();
+    opis.value = "";
+    skladniki.value = "";
+    url.value = "";
+    nazwa.value = "";
   });
-});
-
-// const addRecipe = document.querySelector("#addRecipe");
-
-// .fromTo(contact, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 }, "-=0.1")
-// .fromTo(social, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 }, "-=0.5")
-// .fromTo(logo, 0.2, { color: "white" }, { color: "black" }, "-=1")
-// .fromTo(
-//   hamburgerLines,
-//   0.2,
-//   { stroke: "white" },
-//   { stroke: "black" },
-//   "-=1"
-// );
-
-// addRecipe.addEventListener("click", () => {
-//   tl.reversed() ? tl.play() : tl.reverse();
-// });
